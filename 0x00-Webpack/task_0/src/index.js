74345268639c5d6cce85678533c1f88444da2af7
Loubna <loubna.alfidi@gmail.com>
2024-05-07 00:03:51 +0100

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+import $ from 'jquery';
+
+const paragraphs = [
+    'Holberton Dashboard',
+    'Dashboard data for the students',
+    'Copyright - Holberton School'
+  ];
+  
+  $(document).ready(() => {
+    paragraphs.forEach((text) => {
+      $('body').append(`<p>${text}</p>`);
+    });
+  });
