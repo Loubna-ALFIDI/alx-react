07561153423aaa2167c42603a3b7f2ac909c1ae4
Loubna <loubna.alfidi@gmail.com>
2024-05-08 23:19:43 +0100

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const rootNotification = ReactDOM.createRoot(document.getElementById('root-notifications'));
+rootNotification.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
+);
